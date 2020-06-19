3182848677bd12e3cce55496191b0b4a3ba5dab5
setday <sana3696g@gmail.com>
2020-06-19 20:13:49 +0300

    Fix bug with camera reposition and changing target of the camera

--- src/code/Camera/CameraWork.js
+++ src/code/Camera/CameraWork.js
@@ -4,9 +4,9 @@ import { KeyboardWork, MouseWork } from './../IOWorking/IOWork';
 export default class Camera {
   constructor (fov, near, far, haveMouseWork = false) {
     this._camera = new THREE.PerspectiveCamera(fov, 1, near, far);
-    this._camera.position.set(2, 2, 2);
-    this._camera.lookAt(0, 0, 0);
-    this._target = { x: 0, y: 0, z: 0 };
+    this._camera.position.set(0, 0, 0);
+    this._camera.lookAt(0, 0, -1);
+    this._target = { x: 0, y: 0, z: -1 };
     this._xAngle = 0;
     this._yAngle = 0;
     this._lastxAngle = 0;
@@ -30,9 +30,34 @@ export default class Camera {
     if (value.x !== undefined) { this._camera.position.x = value.x; }
     if (value.y !== undefined) { this._camera.position.y = value.y; }
     if (value.z !== undefined) { this._camera.position.z = value.z; }
+
+    this.targetSetWithoutAngle({
+      x: this._camera.position.x - Math.sin(this._xAngle) * Math.cos(this._yAngle),
+      y: this._camera.position.y + Math.sin(this._yAngle),
+      z: this._camera.position.z - Math.cos(this._xAngle) * Math.cos(this._yAngle)
+    });
   }
 
   targetSet (value) {
+    this._camera.lookAt(value.x !== undefined ? value.x : this._target.x, value.y !== undefined ? value.y : this._target.y, value.z !== undefined ? value.z : this._target.z);
+    if (value.x !== undefined) { this._target.x = value.x; }
+    if (value.y !== undefined) { this._target.y = value.y; }
+    if (value.z !== undefined) { this._target.z = value.z; }
+
+    if (this._hasMouseWork) {
+      const camDirX = this._target.x - this._camera.position.x;
+      const camDirY = this._target.y - this._camera.position.y;
+      const camDirZ = this._target.z - this._camera.position.z;
+
+      this._xAngle = 0;
+      if (camDirZ !== 0 || camDirX !== 0) {
+        this._xAngle = -Math.acos(camDirZ / Math.sqrt(camDirZ ** 2 + camDirX ** 2)) * Math.sign(camDirX);
+      }
+      this._yAngle = Math.asin(camDirY / Math.sqrt(camDirZ ** 2 + camDirX ** 2 + camDirY ** 2));
+    }
+  }
+
+  targetSetWithoutAngle (value) {
     this._camera.lookAt(value.x !== undefined ? value.x : this._target.x, value.y !== undefined ? value.y : this._target.y, value.z !== undefined ? value.z : this._target.z);
     if (value.x !== undefined) { this._target.x = value.x; }
     if (value.y !== undefined) { this._target.y = value.y; }
@@ -54,13 +79,16 @@ export default class Camera {
   update () {
     if (this._hasMouseWork) {
       this._yAngle += this._mouseWork.getYChange / 500;
-      this._xAngle = this._mouseWork.getTransX / 500;
+      this._xAngle += this._mouseWork.getXChange / 500;
       if (this._xAngle !== this._lastXAngle || this._yAngle !== this._lastYAngle) {
-        this.targetSet({
+        this.targetSetWithoutAngle({
           x: this._camera.position.x - Math.sin(this._xAngle) * Math.cos(this._yAngle),
           y: this._camera.position.y + Math.sin(this._yAngle),
           z: this._camera.position.z - Math.cos(this._xAngle) * Math.cos(this._yAngle)
         });
+
+        this._lastYAngle = this._yAngle;
+        this._lastXAngle = this._xAngle;
       }
     }
   }
